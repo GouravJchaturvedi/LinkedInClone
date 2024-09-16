db0d81b26baf911a0adb53bada281881d1949ceb
Gourav Chaturvedi <chaturvedigourav47@gmail.com>
2024-09-16 15:36:59 +0530

    Login completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -7,7 +7,7 @@ export const signup = async (req, res) => {
   try {
     const { name, username, email, password } = req.body;
 
-    if(!name || !username || !email || !password){
+    if (!name || !username || !email || !password) {
       return res.status(401).json({ message: "All fields are required" });
     }
     const existingEmail = await User.findOne({ email });
@@ -33,22 +33,24 @@ export const signup = async (req, res) => {
     const user = new User({ name, username, email, password: hashedPassword });
     await user.save();
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
 
     res.cookie("jwt-linkedIn", token, {
       httpOnly: true,
-      maxAge: 3 * 24 * 60 * 60 * 1000, 
+      maxAge: 3 * 24 * 60 * 60 * 1000,
       sameSite: "strict",
-      secure: process.env.NODE_ENV === "production"
+      secure: process.env.NODE_ENV === "production",
     });
 
     res.status(200).json({ message: "User created successfully" });
 
     const profileUrl = process.env.CLIENT_URL + "/profile/" + user.username;
     try {
-        await sendWelcomeEmail(user.email, user.name , profileUrl);
+      await sendWelcomeEmail(user.email, user.name, profileUrl);
     } catch (emailError) {
-        console.log("Error in sending welcome email", emailError.message);
+      console.log("Error in sending welcome email", emailError.message);
     }
   } catch (error) {
     console.log("Error in signup controller", error.message);
@@ -56,10 +58,38 @@ export const signup = async (req, res) => {
   }
 };
 
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-export const login = async () => {};
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
 
-export const logout = async (req , res) => {
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+
+    await res.cookie("jwt-linkedIn", token, {
+      httpOnly: true,
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+    res.status(200).json({ message: "Logged in successfully" });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = async (req, res) => {
   res.clearCookie("jwt-linkedIn");
   res.status(200).json({ message: "Logged out successfully" });
 };
